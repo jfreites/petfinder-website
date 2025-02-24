166550413971bd99010a88b490ae7aff552bf485
Jonathan Freites <jonathanfreites@gmail.com>
2025-02-24 09:10:44 -0600

    temporary comment a few lines to avoid deploying errors

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,27 +1,28 @@
 import { NextResponse } from 'next/server'
-import chalk from 'chalk'
-import { PutObjectCommand } from '@aws-sdk/client-s3'
-import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
+// import chalk from 'chalk'
+// import { PutObjectCommand } from '@aws-sdk/client-s3'
+// import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 
-import { r2 } from '@/lib/r2'
+//import { r2 } from '@/lib/r2'
 
 export async function POST(request: Request) {
-    try {
-        console.log(chalk.yellow(`Generating an upload URL!`))
+    console.log(request);
+    // try {
+    //     console.log(chalk.yellow(`Generating an upload URL!`))
 
-        const signedUrl = await getSignedUrl(
-            r2,
-            new PutObjectCommand({
-                Bucket: process.env.R2_BUCKET_NAME,
-                Key: `filename.pdf`,
-            }),
-            { expiresIn: 60 }
-        )
+    //     const signedUrl = await getSignedUrl(
+    //         r2,
+    //         new PutObjectCommand({
+    //             Bucket: process.env.R2_BUCKET_NAME,
+    //             Key: `filename.pdf`,
+    //         }),
+    //         { expiresIn: 60 }
+    //     )
 
-        console.log(chalk.green(`Success generating upload URL!`))
+    //     console.log(chalk.green(`Success generating upload URL!`))
 
-        return NextResponse.json({ url: signedUrl })
-    } catch (error) {
-        console.log('error', error);
-    }
+    //     return NextResponse.json({ url: signedUrl })
+    // } catch (error) {
+    //     console.log('error', error);
+    // }
 }
